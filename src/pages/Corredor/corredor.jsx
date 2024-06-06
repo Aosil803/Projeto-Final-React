903edb4e8a2ee9e6545928cf60f765d71165877d
Bianco <arthnilo@gmail.com>
2024-06-05 22:16:57 -0300

    feat: entrada e corredor com links

--- src/pages/Corredor/corredor.jsx
+++ src/pages/Corredor/corredor.jsx
@@ -1,11 +1,44 @@
+import { useNavigate } from 'react-router-dom';
 import corredor from "../../assets/corredor.png"
 import style from "./corredor.module.css"
 
-
 export const Corredor = () => {
 
+  const navigate = useNavigate();
+
+
+  function PortaCrime() {
+    navigate('../crime');
+  }
+  function PortaBonas() {
+    navigate('../portaBonas');
+  }
+  function PortaAlex() {
+    navigate('../portaAlex');
+  }
+  function PortaYuka() {
+    navigate('../portaYuka');
+  }
+  function PortaMiguelito() {
+    navigate('../portaMiguelito');
+  }
+  function PortaLorencini() {
+    navigate('../portaLorencini');
+  }
+  function Home() {
+    navigate('../home');
+  }
 
   return <div>
-      <img className={style.corredor} src={corredor} alt="corredor de uma casa" />
+        <div>
+        <img className={style.corredor} src={corredor} alt="corredor de uma casa" />
+        </div>
+        <button className={style.portaCrime} onClick={PortaCrime}></button>
+        <button className={style.portaBonas} onClick={PortaBonas}></button>
+        <button className={style.portaAlex} onClick={PortaAlex}></button>
+        <button className={style.portaYuka} onClick={PortaYuka}></button>
+        <button className={style.portaMiguelito} onClick={PortaMiguelito}></button>
+        <button className={style.portaLorencini} onClick={PortaLorencini}></button>
+        <button className={style.home} onClick={Home}></button>
   </div>
 };
