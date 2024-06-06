efc3a37264cc56d1c3baf024968fc4c195f414d2
Bianco <arthnilo@gmail.com>
2024-06-06 16:21:37 -0300

    Feat: popUps

--- src/pages/Quarto03/quarto03.jsx
+++ src/pages/Quarto03/quarto03.jsx
@@ -2,8 +2,13 @@ import { useNavigate } from 'react-router-dom';
 import quarto03 from "../../assets/quartoYuka.png"
 import style from "./quarto03.module.css"
 import home from "../../assets/home.gif"
-export const QuartoYuka = () => {
+import { useState } from 'react';
 
+
+export const QuartoYuka = () => {
+  const [openPopupCaixa ,setOpenPopupCaixa] = useState(false)
+  const [openPopupGato ,setOpenPopupGato] = useState(false)
+  const [openPopupSangue ,setOpenPopupSangue] = useState(false)
   const navigate = useNavigate();
 
   function Home() {
@@ -16,15 +21,47 @@ export const QuartoYuka = () => {
     <img className={style.quarto03} src={quarto03} alt="Quarto da yuka" />
     </div>
     <p className={style.tituloLocal}>Quarto Yuka</p>
-    <button className={style.caixa}></button>
-    <button className={style.gato}></button>
-    <button className={style.sangue}></button>
+    <button className={style.caixa} onClick={() => setOpenPopupCaixa(!openPopupCaixa)}></button>
+    <button className={style.gato} onClick={() => setOpenPopupGato(!openPopupGato)}></button>
+    <button className={style.sangue} onClick={() => setOpenPopupSangue(!openPopupSangue)}></button>
     <button className={style.home} onClick={Home}></button>
-        <div className={style.containerIcon}>
-          <a href="#" onClick={Home}>
-            <img className={style.homeIcon} src={home} alt="icon home" />
-          </a>
-        </div>
+    <div className={style.containerIcon}>
+      <a href="#" onClick={Home}>
+        <img className={style.homeIcon} src={home} alt="icon home" />
+        </a>
+    </div>
 
+    {
+      openPopupCaixa &&
+    <div className={style.caixaPopUp}>
+      <div className={style.caixaPopUpInterna}>
+        <h2>PopUp</h2>
+        <button onClick={()=> setOpenPopupCaixa(false)}>X</button>
+      </div>
+      <p className={style.textoPopUp}>Caixa PopUp </p>
+    </div>
+    }
+
+    {
+      openPopupGato &&
+    <div className={style.gatoPopUp}>
+      <div className={style.caixaPopUpInterna}>
+        <h2>PopUp</h2>
+        <button onClick={()=> setOpenPopupGato(false)}>X</button>
+      </div>
+      <p className={style.textoPopUp}>Gato PopUp </p>
+    </div>
+    }
+
+    {
+      openPopupSangue &&
+    <div className={style.sanguePopUp}>
+      <div className={style.caixaPopUpInterna}>
+        <h2>PopUp</h2>
+        <button onClick={()=> setOpenPopupSangue(false)}>X</button>
+      </div>
+      <p className={style.textoPopUp}>muito sangue pop PopUp </p>
+    </div>
+    }
   </div>
 };
