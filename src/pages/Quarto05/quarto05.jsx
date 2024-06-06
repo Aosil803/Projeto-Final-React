e7873384979f7c2327043004526e589266f4f3a5
Bianco <arthnilo@gmail.com>
2024-06-06 18:37:03 -0300

    feat: pop up quarto miguelito

--- src/pages/Quarto05/quarto05.jsx
+++ src/pages/Quarto05/quarto05.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import quarto05 from "../../assets/quartoMiguelito.png"
 import style from "./quarto05.module.css"
 import home from "../../assets/home.gif"
@@ -6,6 +7,9 @@ import home from "../../assets/home.gif"
 
 export const QuartoMiguelito = () => {
   const navigate = useNavigate();
+  const [openPopupSangue ,setOpenPopupSangue] = useState(false)
+  const [openPopupEvidencia ,setOpenPopupEvidencia] = useState(false)
+  const [openPopupComputador ,setOpenPopupComputador] = useState(false)
 
   function Home() {
     navigate('../corredor');
@@ -20,8 +24,42 @@ export const QuartoMiguelito = () => {
             <img className={style.homeIcon} src={home} alt="icon home" />
           </a>
         </div>
-    <button className={style.brilho}></button>
-    <button className={style.monitor}></button>
-    <button className={style.sangue}></button>
+    <button className={style.brilho} onClick={() => setOpenPopupEvidencia(!openPopupEvidencia)}></button>
+    <button className={style.monitor} onClick={() => setOpenPopupComputador(!openPopupComputador)}></button>
+    <button className={style.sangue} onClick={() => setOpenPopupSangue(!openPopupSangue)}></button>
+
+    {
+      openPopupSangue &&
+    <div className={style.sanguePopUp}>
+      <div className={style.caixaPopUpInterna}>
+        <h2>PopUp</h2>
+        <button onClick={()=> setOpenPopupSangue(false)}>X</button>
+      </div>
+      <p className={style.textoPopUp}>Caixa PopUp </p>
+    </div>
+    }
+
+    {
+      openPopupEvidencia &&
+    <div className={style.evidenciaPopUp}>
+      <div className={style.caixaPopUpInterna}>
+        <h2>PopUp</h2>
+        <button onClick={()=> setOpenPopupEvidencia(false)}>X</button>
+      </div>
+      <p className={style.textoPopUp}>Caixa PopUp </p>
+    </div>
+    }
+
+{
+      openPopupComputador &&
+    <div className={style.computadorPopUp}>
+      <div className={style.caixaPopUpInterna}>
+        <h2>PopUp</h2>
+        <button onClick={()=> setOpenPopupComputador(false)}>X</button>
+      </div>
+      <p className={style.textoPopUp}>Caixa PopUp </p>
+    </div>
+    }
+
   </div>
 };
