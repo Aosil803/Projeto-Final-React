486f9fc2cd463b5359ebc61006bd63894cd6de10
Bianco <arthnilo@gmail.com>
2024-06-06 17:48:40 -0300

    feat: pop up quarto Alex

--- src/pages/Quarto01/quarto01.jsx
+++ src/pages/Quarto01/quarto01.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import quarto01 from "../../assets/quartoAlex.png"
 import style from "./quarto01.module.css"
-import { useNavigate } from 'react-router-dom';
 import home from "../../assets/home.gif"
 
 
 export const QuartoAlex = () => {
   const navigate = useNavigate();
-
+  const [openPopupCofre ,setOpenPopupCofre] = useState(false)
+  const [openPopupGaveta ,setOpenPopupGaveta] = useState(false)
+  const [openPopupChave ,setOpenPopupChave] = useState(false)
   function Home() {
     navigate('../corredor');
   }
@@ -23,8 +26,44 @@ export const QuartoAlex = () => {
             <img className={style.homeIcon} src={home} alt="icon home" />
           </a>
         </div>
-    <button className={style.caixaArmario}></button>
-    <button className={style.gaveta}></button>
-    <button className={style.chave}></button>
+    <button className={style.caixaArmario} onClick={() => setOpenPopupCofre(!openPopupCofre)}></button>
+    <button className={style.gaveta} onClick={() => setOpenPopupGaveta(!openPopupGaveta)}></button>
+    <button className={style.chave} onClick={() => setOpenPopupChave(!openPopupChave)}></button>
+
+    {
+      openPopupCofre &&
+    <div className={style.cofrePopUp}>
+      <div className={style.caixaPopUpInterna}>
+        <h2>PopUp</h2>
+        <button onClick={()=> setOpenPopupCofre(false)}>X</button>
+      </div>
+      <p className={style.textoPopUp}>Caixa PopUp </p>
+    </div>
+    }
+
+    {
+      openPopupGaveta &&
+    <div className={style.gavetaPopUp}>
+      <div className={style.caixaPopUpInterna}>
+        <h2>PopUp</h2>
+        <button onClick={()=> setOpenPopupGaveta(false)}>X</button>
+      </div>
+      <p className={style.textoPopUp}>Caixa PopUp </p>
+    </div>
+    }
+
+    {
+      openPopupChave &&
+    <div className={style.chavePopUp}>
+      <div className={style.caixaPopUpInterna}>
+        <h2>PopUp</h2>
+        <button onClick={()=> setOpenPopupChave(false)}>X</button>
+      </div>
+      <p className={style.textoPopUp}>Caixa PopUp </p>
+    </div>
+    }
+
+
+
   </div>
 };
