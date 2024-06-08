95c4169c35390636db5d28a1e3768b03acf61850
Bianco <arthnilo@gmail.com>
2024-06-07 21:28:04 -0300

    feat: timer popUp

--- src/pages/Entrada/entrada.jsx
+++ src/pages/Entrada/entrada.jsx
@@ -1,12 +1,28 @@
-import  Button from '../../components/button'
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import  Button from '../../components/button'
 import entrada1 from "../../assets/entrada.png"
 import style from "./entrada.module.css";
+import PopUp from '../../components/PopUp/popUp.jsx'
+import retangulo from "../../assets/Retangulo.png"
 
 export const Entrada = () => {
-
+  const [openPopup, setOpenPopup] = useState(false);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const openTimer = setTimeout(() => {
+      setOpenPopup(true);
+
+      const closeTimer = setTimeout(() => {
+        setOpenPopup(false);
+      }, 5000);
+
+      return () => clearTimeout(closeTimer);
+    }, 3000);
+
+    return () => clearTimeout(openTimer);
+  }, []);
 
   function Corredor() {
     navigate('../corredor');
@@ -18,8 +34,15 @@ export const Entrada = () => {
         </div>
         <Button title="" className={style.portaEntrada} onClick={Corredor}/>
         
-        <div>
+        <PopUp  style={{position:"absolute", top: 150, left: 460}}
+        open={openPopup}
+        onClose={() => setOpenPopup(false)}
+        title="Porteira"
+      >
+        <img src={retangulo} alt="Porteira" />
+      <p>Atrás dela há uma porta trancada.</p>
+      </PopUp>
 
-        </div>
+    
   </div>
 };
